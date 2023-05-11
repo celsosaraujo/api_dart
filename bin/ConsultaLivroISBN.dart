db4ef26d7a9379698cea4d3dcf2f9cebc2ebfe26
Celso Serrano Araujo <celso.saraujo@hotmail.com>
2023-05-11 19:03:48 -0300

    correção da consulta do ISBN

--- bin/ConsultaLivroISBN.dart
+++ bin/ConsultaLivroISBN.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
-void main() async {
-  final isbn = 'urn:isbn:9780140328721';
+Future<void> main() async {
+  final isbn = '9780140328721';
   final url = Uri.parse('https://openlibrary.org/api/books?bibkeys=$isbn&format=json&jscmd=data');
   final response = await http.get(url);
 
